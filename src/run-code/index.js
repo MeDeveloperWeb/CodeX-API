b0257425b536750a46ba8929f33b719ee4ea2a76
Prakash <63726119+MeDeveloperWeb@users.noreply.github.com>
2024-04-21 16:28:27 +0530

    Undo test2 commit

--- src/run-code/index.js
+++ src/run-code/index.js
@@ -7,13 +7,11 @@ const {spawn} = require("child_process");
 
 async function runCode({language = "", code = "", input = ""}) {
     const timeout = 30;
-    console.log(code)
     if (code === "")
         throw {
             status: 400,
             error: "No Code found to execute."
         }
-    console.log(code)
 
     if (!supportedLanguages.includes(language))
         throw {
